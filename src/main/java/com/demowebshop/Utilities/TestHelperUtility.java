014c424fe46e4900d487b52609da66efbab758a5
lijisha123 <lijishanair23@gmail.com>
2023-04-05 12:21:22 +0400

    adding initial script

--- src/main/java/com/demowebshop/Utilities/TestHelperUtility.java
+++ src/main/java/com/demowebshop/Utilities/TestHelperUtility.java
@@ -1,5 +1,10 @@
 package com.demowebshop.Utilities;
 
 public class TestHelperUtility {
+	public PageUtility page=new PageUtility();
+    public Tableutility table=new Tableutility();
+    public FileUploadUtility upload=new FileUploadUtility();
+    public RandomUtility random=new RandomUtility();
+    public WaitUtility wait = new WaitUtility();
 
 }
